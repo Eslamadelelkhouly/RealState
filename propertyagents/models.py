eff48814ad879cead8f32ba92de76bbf59e7e6d5
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-28 01:43:07 +0300

    add models propertyagents

--- propertyagents/models.py
+++ propertyagents/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class PropertyAgents(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='agentsperson/')
+    facebooklink = models.URLField(max_length=200)
+    instagramlink = models.URLField(max_length=200)
+    twittlink = models.URLField(max_length=200)
