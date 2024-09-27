85a2ea4347186b15859fd3a53f2d583491de6655
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-28 01:38:45 +0300

    add Contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    address = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254)
+    facebooklink = models.URLField(max_length=200)
+    instagramlink = models.URLField(max_length=200)
+    yotubelink = models.URLField(max_length=200)
+    linkedlnlink = models.URLField(max_length=200)
